83ea3c9ae8e2a7ab8a421b65c2b44dfd19c8afa2
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2025-03-06 19:51:58 -0600

    Added materials for spaces

--- Assets/Scripts/Space.cs
+++ Assets/Scripts/Space.cs
@@ -1,15 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
 /// Holds some general information of a space.
 /// </summary>
-[RequireComponent(typeof(NextSpaceProvider))]
+[RequireComponent(typeof(NextSpaceProvider), typeof(MeshRenderer))]
 public class Space : MonoBehaviour
 {
     /// <summary>
     /// Represents whether or not the space is on fire.
     /// </summary>
-    public bool IsOnFire;
+    private bool _isOnFire;
+
+    private Renderer _renderer;
+    private Material _baseMaterial;
+    [SerializeField] private Material _fireMaterial;
+
+	public void Awake()
+	{
+		_renderer = GetComponent<MeshRenderer>();
+        _baseMaterial = _renderer.material;
+	}
+
+    public bool IsOnFire
+    {
+        get => _isOnFire;
+        set
+        {
+            _isOnFire = value;
+            if (_isOnFire)
+			{
+                _renderer.materials = new Material[] { _baseMaterial, _fireMaterial };
+            }
+            else
+			{
+				_renderer.materials = new Material[] { _baseMaterial };
+			}
+        }
+    }
 
     /// <summary>
     /// Represents the space's biome.

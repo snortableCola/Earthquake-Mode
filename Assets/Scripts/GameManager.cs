59bc04218f6f505c939059621c5df10e02e490e1
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2025-03-13 22:38:50 -0500

    Commented game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,34 @@ using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-	[SerializeField] private Player[] _players;
+	private Player[] _players;
+	[SerializeField] private Transform _board;
+
+	private void Awake()
+	{
+		_players = _board.GetComponentsInChildren<Player>();
+	}
 
 	private int _currentPlayerIndex;
 	private int _roundNumber = 1;
 
+	/// <summary>
+	/// Event that triggers whenever a turn is completed, providing the player whose turn passed.
+	/// </summary>
 	public UnityEvent<Player> TurnPassed = new();
+
+	/// <summary>
+	/// Event that triggers whenever a full round of play for all players has completed.
+	/// </summary>
 	public UnityEvent RoundPassed = new();
 
 	[ContextMenu("Move Next Player")]
 	private void MoveNextPlayer() => StartCoroutine(NextPlayerTurnCoroutine());
 
+	/// <summary>
+	/// Coroutine representing the next player taking their turn.
+	/// </summary>
+	/// <returns>Coroutine for handling the next player's turn.</returns>
 	private IEnumerator NextPlayerTurnCoroutine()
 	{
 		Player player = _players[_currentPlayerIndex];

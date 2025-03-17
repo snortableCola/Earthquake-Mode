58c6da308c87e4c4c1e97d034626d249222116b9
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2025-03-17 17:00:13 -0500

    Simplified Next-Space Behaviors

--- Assets/Scripts/Spaces/NextSpaceProvider.cs
+++ Assets/Scripts/Spaces/NextSpaceProvider.cs
@@ -1,17 +1,11 @@
 using UnityEngine;
 
-/// <summary>
-/// Abstract behavior of a space, responsible for deciding whatever space comes after it during a player's movement.
-/// </summary>
-public abstract class NextSpaceProvider : MonoBehaviour
+public class NextSpaceProvider : MonoBehaviour
 {
-	/// <summary>
-	/// Provides the next space during a player's movement.
-	/// </summary>
-	public abstract Space NextSpace { get; }
+	[SerializeField] private Space[] _nextSpaces;
 
 	/// <summary>
 	/// Provides all spaces this space could possibly lead into during a player's movement.
 	/// </summary>
-	public abstract Space[] NextSpaces { get; }
+	public Space[] NextSpaces => _nextSpaces;
 }

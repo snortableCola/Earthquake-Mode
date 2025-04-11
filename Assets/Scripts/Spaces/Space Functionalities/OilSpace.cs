c8dd9f00bdaea91310bc8cd280b8427105d8d207
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2025-04-10 23:04:01 -0500

    Oil Space can now be "active" and passable or "inactive" and landable

--- Assets/Scripts/Spaces/Space Functionalities/OilSpace.cs
+++ Assets/Scripts/Spaces/Space Functionalities/OilSpace.cs
@@ -1,12 +1,41 @@
 using System.Collections;
 using UnityEngine;
 
-public class OilSpace : SpaceBehavior
+public class OilSpace : ResourceSpace
 {
+	private readonly Vector3 _activeScale = new(0.2f, 0.05f, 0.2f);
+	private readonly Vector3 _inactiveScale = new(0.35f, 0.05f, 0.35f);
+
+	private bool _isActive;
+	public bool IsActive
+	{
+		get => _isActive;
+		set
+		{
+			_isActive = value;
+			transform.localScale = value ? _activeScale : _inactiveScale;
+		}
+	}
+
+	public override bool EndsTurn => !_isActive;
+
+	[ContextMenu("Flip Oil Space State")]
+	public void FlipActivity() => IsActive = !_isActive;
+
 	public override IEnumerator RespondToPlayer(Player player)
 	{
-		Debug.Log($"{player.name} landed on an oil space.");
+		if (_isActive)
+		{
+			Debug.Log($"{player.name} passed on an active oil space.");
 
-		yield return DisasterManager.Instance.IncrementEarthquake();
+			yield return DisasterManager.Instance.IncrementEarthquake();
+		}
+
+		else
+		{
+			Debug.Log($"{player.name} landed on an inactive oil space.");
+
+			yield return base.RespondToPlayer(player);
+		}
 	}
 }

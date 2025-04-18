46c01178407e9ba81c799227ce239121756abd04
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2025-04-18 15:18:49 -0500

    Added sabotage button logic

--- Assets/Scripts/Spaces/General Space Behaviors/Sabotage Manager.cs
+++ Assets/Scripts/Spaces/General Space Behaviors/Sabotage Manager.cs
@@ -8,30 +8,41 @@ public class SabotageManager : MonoBehaviour
 {
 	public static SabotageManager Instance { get; private set; }
 
-	[SerializeField] private GameObject _victimSelectPanel;
-	[SerializeField] private GameObject _methodSelectPanel;
-	[SerializeField] private GameObject _consequencePanel;
-
-	[SerializeField] private Button[] _playerSelectButtons;
-	private int _selectedPlayerIndex;
-
 	[SerializeField] private int MaxCoinsToSteal;
 	[SerializeField] private int StealingOilCost;
+
+	[SerializeField] private GameObject _victimSelectPanel;
+	[SerializeField] private Button[] _victimSelectButtons;
+	private int _selectedVictimIndex;
+
+	[SerializeField] private GameObject _methodSelectPanel;
+	[SerializeField] private Button _stealCoinsButton;
+	[SerializeField] private Button _stealOilButton;
+	private bool? _stealingOil;
+
+	[SerializeField] private GameObject _consequencePanel;
+	[SerializeField] private Button _exitSabotageButton;
+	private bool _exiting;
 
 	private void Awake()
 	{
 		Instance = this;
 
-		if (_playerSelectButtons.Length != GameManager.Instance.Players.Length)
+		if (_victimSelectButtons.Length != GameManager.Instance.Players.Length)
 		{
 			Debug.LogError("The sabotage manager doesn't have the correct amount of victim-selection buttons");
 		}
 
-		for (int i = 0; i < _playerSelectButtons.Length; i++)
+		for (int i = 0; i < _victimSelectButtons.Length; i++)
 		{
-			_playerSelectButtons[i].onClick.AddListener(SetSelectedPlayer);
-			void SetSelectedPlayer() => _selectedPlayerIndex = i;
+			_victimSelectButtons[i].onClick.AddListener(SetSelectedPlayer);
+			void SetSelectedPlayer() => _selectedVictimIndex = i;
 		}
+
+		_stealCoinsButton.onClick.AddListener(() => _stealingOil = false);
+		_stealOilButton.onClick.AddListener(() => _stealingOil = true);
+
+		_exitSabotageButton.onClick.AddListener(() => _exiting = true);
 	}
 
 	public IEnumerator Sabotage(Player saboteur)
@@ -44,24 +55,26 @@ public class SabotageManager : MonoBehaviour
 		PanelManager.Instance.ShowPanel(_victimSelectPanel);
 
 		// Wait for selection (for sake of implementation, this is how to get a random victim)
-		_selectedPlayerIndex = -1;
-		yield return new WaitUntil(() => _selectedPlayerIndex != -1);
+		_selectedVictimIndex = -1;
+		yield return new WaitUntil(() => _selectedVictimIndex != -1);
 
 		// Use the index of the selected victim button to get an actual victim player object
-		if (_selectedPlayerIndex == otherPlayers.Count) _selectedPlayerIndex = Random.Range(0, otherPlayers.Count); // Last button counts as random
-		Player selectedVictim = otherPlayers[_selectedPlayerIndex];
+		if (_selectedVictimIndex == otherPlayers.Count) _selectedVictimIndex = Random.Range(0, otherPlayers.Count); // Last button counts as random
+		Player selectedVictim = otherPlayers[_selectedVictimIndex];
 
 		// Determine whether or not it's possible for the saboteur to steal oil
 		bool canStealOil = selectedVictim.Oil > 0 && saboteur.Coins >= StealingOilCost;
+		_stealOilButton.interactable = canStealOil;
 
 		// Display choice between stealing coins or oil
 		PanelManager.Instance.ShowPanel(_methodSelectPanel);
 
 		// Wait for selection (for this, let's just say if they can steal oil, they will steal oil)
-		bool wantsOil = canStealOil;
+		_stealingOil = null;
+		yield return new WaitUntil(() => _stealingOil != null);
 
 		// Handle the logic for taking away oil or coins depending on selection
-		if (wantsOil)
+		if (_stealingOil == true)
 		{
 			selectedVictim.Oil--;
 			saboteur.Oil++;
@@ -77,7 +90,7 @@ public class SabotageManager : MonoBehaviour
 		PanelManager.Instance.ShowPanel(_consequencePanel);
 
 		// Wait for consequence to be canceled out of
-
-		yield break;
+		_exiting = false;
+		yield return new WaitUntil(() => _exiting);
 	}
 }

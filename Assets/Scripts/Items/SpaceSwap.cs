66ce799fe71f15d5d6f686bfb4a0470d9a1e3f8c
Jett McWhorter <158016844+Jettmcw@users.noreply.github.com>
2025-05-09 13:29:53 -0500

    Fixed bugs with disasters & space swap for low players

--- Assets/Scripts/Items/SpaceSwap.cs
+++ Assets/Scripts/Items/SpaceSwap.cs
@@ -13,11 +13,14 @@ public class SpaceSwap : Item
 		player.UsedItem = this;
 
 		Player[] players = GameManager.Instance.Players;
+		
 
 		// Randomly selected player, can be given UI for player choice
 		int targetIndex = Random.Range(0, players.Length - 1);
 		Player target = players[targetIndex];
 		if (target == player) target = players[^1];
+
+		if (target.Movement == null) yield break;
 
 		Space start = player.GetComponentInParent<Space>();
 		Space end = target.GetComponentInParent<Space>();
